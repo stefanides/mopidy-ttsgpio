26633a185d9d81839b0679a64b605217be4f7a23
Jan Stefanides <jan.stefanides@gmail.com>
2016-12-09 21:16:33 +0100

    gpio_input_manager: cleanup duplicate code, add debug logging to GPIO events

--- mopidy_ttsgpio/gpio_input_manager.py
+++ mopidy_ttsgpio/gpio_input_manager.py
@@ -4,8 +4,8 @@ import time
 import RPi.GPIO as GPIO
 
 logger = logging.getLogger(__name__)
-longpress_time = 0.3
-
+longpress_time = 0.3 # [seconds]
+bounce_time = 20     # [miliseconds]
 
 class GPIOManager():
 
@@ -34,32 +34,32 @@ class GPIOManager():
             GPIO.setup(pins['pin_button_next'], GPIO.IN,
                        pull_up_down=GPIO.PUD_UP)
             GPIO.add_event_detect(pins['pin_button_next'],
-                                  GPIO.BOTH, callback=self.next, bouncetime=30)
+                                  GPIO.BOTH, callback=self.next, bouncetime=bounce_time)
 
             # Previous Button
             GPIO.setup(pins['pin_button_previous'], GPIO.IN,
                        pull_up_down=GPIO.PUD_UP)
             GPIO.add_event_detect(pins['pin_button_previous'], GPIO.BOTH,
-                                  callback=self.previous, bouncetime=30)
+                                  callback=self.previous, bouncetime=bounce_time)
 
             # Volume Up Button
             GPIO.setup(pins['pin_button_vol_up'], GPIO.IN,
                        pull_up_down=GPIO.PUD_UP)
             GPIO.add_event_detect(pins['pin_button_vol_up'], GPIO.BOTH,
-                                  callback=self.vol_up, bouncetime=30)
+                                  callback=self.vol_up, bouncetime=bounce_time)
 
             # Volume Down Button
             GPIO.setup(pins['pin_button_vol_down'], GPIO.IN,
                        pull_up_down=GPIO.PUD_UP)
             GPIO.add_event_detect(pins['pin_button_vol_down'],
                                   GPIO.BOTH, callback=self.vol_down,
-                                  bouncetime=30)
+                                  bouncetime=bounce_time)
 
             # Main Button
             GPIO.setup(pins['pin_button_main'], GPIO.IN,
                        pull_up_down=GPIO.PUD_UP)
             GPIO.add_event_detect(pins['pin_button_main'],
-                                  GPIO.BOTH, callback=self.main, bouncetime=30)
+                                  GPIO.BOTH, callback=self.main, bouncetime=bounce_time)
 
             self.correctlyLoaded = True
 
@@ -68,60 +68,34 @@ class GPIOManager():
                          "to use GPIO. GPIO input will not work")
 
     def set_led(self, led_state):
+        logger.debug("LED:"+str(led_state))
         if self.correctlyLoaded:
             GPIO.output(self.led_pin, led_state)
 
-    def previous(self, channel):
-        if GPIO.input(channel) == 1:
-            if self.down_time_previous >= 0:
-                if self.down_time_previous + longpress_time > time.time():
-                    self.frontend.input({'key': 'previous', 'long': False})
-                else:
-                    self.frontend.input({'key': 'previous', 'long': True})
-            self.down_time_previous = -1
-        else:
-            self.down_time_previous = time.time()
-
-    def next(self, channel):
-        if GPIO.input(channel) == 1:
+    def handle_event(self, channel, event):
+        gpio = GPIO.input(channel)
+        logger.debug(event+":"+str(gpio))
+        if gpio == 1:
             if self.down_time_next >= 0:
                 if self.down_time_next + longpress_time > time.time():
-                    self.frontend.input({'key': 'next', 'long': False})
+                    self.frontend.input({'key': event, 'long': False})
                 else:
-                    self.frontend.input({'key': 'next', 'long': True})
+                    self.frontend.input({'key': event, 'long': True})
             self.down_time_next = -1
         else:
             self.down_time_next = time.time()
 
+    def previous(self, channel):
+        self.handle_event(channel, 'previous')
+
+    def next(self, channel):
+        self.handle_event(channel, 'next')
+
     def main(self, channel):
-        if GPIO.input(channel) == 1:
-            if self.down_time_main >= 0:
-                if self.down_time_main + longpress_time > time.time():
-                    self.frontend.input({'key': 'main', 'long': False})
-                else:
-                    self.frontend.input({'key': 'main', 'long': True})
-            self.down_time_main = -1
-        else:
-            self.down_time_main = time.time()
+        self.handle_event(channel, 'main')
 
     def vol_up(self, channel):
-        if GPIO.input(channel) == 1:
-            if self.down_time_vol_up >= 0:
-                if self.down_time_vol_up + longpress_time > time.time():
-                    self.frontend.input({'key': 'volume_up', 'long': False})
-                else:
-                    self.frontend.input({'key': 'volume_up', 'long': True})
-            self.down_time_vol_up = -1
-        else:
-            self.down_time_vol_up = time.time()
+        self.handle_event(channel, 'volume_up')
 
     def vol_down(self, channel):
-        if GPIO.input(channel) == 1:
-            if self.down_time_vol_down >= 0:
-                if self.down_time_vol_down + longpress_time > time.time():
-                    self.frontend.input({'key': 'volume_down', 'long': False})
-                else:
-                    self.frontend.input({'key': 'volume_down', 'long': True})
-            self.down_time_vol_down = -1
-        else:
-            self.down_time_vol_down = time.time()
+        self.handle_event(channel, 'volume_down')
